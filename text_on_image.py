a2b7825734010b252fc5e22ed47a1a8f61da6a8b
Urvashi Shasne <145223767+Urvashishasne@users.noreply.github.com>
2024-08-23 17:21:45 +0530

    Add files via upload

---
+++ text_on_image.py
@@ -0,0 +1,18 @@
+import cv2 
+import numpy 
+
+img1 = cv2.imread('cv2 work\\images\\nature.jpg')
+img1= cv2.resize(img1,(500,500)) 
+
+txt=cv2.putText(img = img1,
+text = "Urvi",
+org = (60,100), #for downside of img make y axis value greater 
+fontFace = cv2.FONT_HERSHEY_DUPLEX ,
+fontScale = 3 ,
+color = (0,0,255),
+thickness = 3,
+lineType = cv2.LINE_8,
+bottomLeftOrigin = False) 
+cv2.imshow("text_image",txt)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
